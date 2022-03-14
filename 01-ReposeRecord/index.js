198cdd79bd97c81a6c015ef4ed957c4c17935570
Andrea Catania <andreacatania91@gmail.com>
2022-03-14 16:20:51 +0000

    calculated most sleeper

--- 01-ReposeRecord/index.js
+++ 01-ReposeRecord/index.js
@@ -1,5 +1,35 @@
 const fs = require("fs");
 
-const patterns = fs.readFileSync("./input.txt", "utf-8").split("\n").sort();
+const guardsLogs = fs.readFileSync("./input.txt", "utf-8").split("\n").sort();
 
-console.log(patterns);
+const guardsSleepMins = {};
+let actualGuard = null;
+let sleepTime = 0;
+let wakeTime = 0;
+
+guardsLogs.forEach((log) => {
+  const matchGuards = /#\d*/g.exec(log);
+  const matchMinutes = /:+(\d{2})/g.exec(log);
+  let sleepCircle = false;
+  if (matchGuards) {
+    if (!guardsSleepMins[matchGuards[0]]) guardsSleepMins[matchGuards[0]] = 0;
+    actualGuard = matchGuards[0];
+  }
+  if (matchMinutes && log.includes("asleep")) {
+    sleepTime = matchMinutes[1];
+  }
+  if (matchMinutes && log.includes("wakes")) {
+    sleepCircle = true;
+    wakeTime = matchMinutes[1];
+  }
+  if (sleepCircle) {
+    const totalSleep = wakeTime - sleepTime - 1;
+    guardsSleepMins[actualGuard] += totalSleep;
+  }
+});
+
+const mostSleeperGuard = Object.entries(guardsSleepMins).sort(
+  ([, a], [, b]) => b - a
+)[0][0];
+
+console.log(mostSleeperGuard);
